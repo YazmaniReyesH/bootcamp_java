789584698453a4adc338a9c3e3faa93cf21435f1
YazmaniReyesH <yazmani.reyesh@gmail.com>
2023-06-06 15:02:16 -0600

    Se sube el código para el programa de "Calculadora de Edad de Perros"

---
+++ Clase_5/Main.java
@@ -0,0 +1,20 @@
+package Clase_5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int edadPerro = 0;
+        Scanner IngresaEdad = new Scanner(System.in);
+
+        System.out.println("Ingresa la edad de tu perro en años humanos:");
+        int edadHumana = IngresaEdad.nextInt();
+
+        if (edadHumana <= 0) {
+            System.out.println("La edad debe ser un número positivo.");
+        } else {
+            edadPerro = edadHumana * 7;
+        }
+        System.out.println("La edad de tu perro en años de perro es: " + edadPerro);
+    }
+}
