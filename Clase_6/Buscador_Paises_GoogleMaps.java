92096da390a79ce32b72b0528db3db545cec1d68
YazmaniReyesH <yazmani.reyesh@gmail.com>
2023-06-08 15:39:13 -0600

    Se suben a la Clase 6 del repositorio el programa buscardor de Tweets para Twitter.com: "Buscador_Tweets.java"

--- Clase_6/Buscador_Paises_GoogleMaps.java
+++ Clase_6/Buscador_Paises_GoogleMaps.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Buscador_Paises_GoogleMaps {
     public static void main(String[] args) {
-        //Buscador de países con Google Maps
-        Scanner ingresaPais = new Scanner(System.in);
-        System.out.println("Ingresa el pais: ");
-        String pais = ingresaPais.next();
-        System.out.println("https://www.google.com/maps/search/" + pais);
+        //Buscador de Tweets para Twitter.com
+        Scanner ingresaCelebridad = new Scanner(System.in);
+        System.out.println("Ingresa el nombre de una celebridad (una sola palabra): ");
+        String celebridad = ingresaCelebridad.next();
+        System.out.println("https://twitter.com/search?q=" + celebridad);
     }
 }
