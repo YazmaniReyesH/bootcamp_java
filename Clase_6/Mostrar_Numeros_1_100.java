d96334f2119db3013c33e7b267b625ad42b26a9b
YazmaniReyesH <yazmani.reyesh@gmail.com>
2023-06-08 19:47:35 -0600

    Se sube a la Clase 6 del repositorio, el programa para que muestra los números del 1 al 100 utilizando un bucle while: "Mostrar_Numeros_1_100.java"

---
+++ Clase_6/Mostrar_Numeros_1_100.java
@@ -0,0 +1,13 @@
+package Clase_6;
+
+public class Mostrar_Numeros_1_100 {
+    public static void main(String[] args) {
+        //Muestra los números del 1 al 100 utilizando un bucle while.
+
+        int numero = 0;
+        while (numero < 100){
+            numero++;
+            System.out.println("Numero: " + numero);
+        }
+    }
+}
