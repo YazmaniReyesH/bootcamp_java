a45dc2716d0615ff288e757e4dee1b425cc44ae9
YazmaniReyesH <yazmani.reyesh@gmail.com>
2023-06-15 03:02:00 -0600

    Se actualiza repositorio del proyecto de Springboot bootcampjava.

---
+++ Clase_8/bootcampjava/src/test/java/yazmani/reyesh/bootcampjava/BootcampjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package yazmani.reyesh.bootcampjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
